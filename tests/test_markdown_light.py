dfa7eb0ad312024fa5842e2f1493f2fb5bd0fe19
evandrocoan <evandrocoan@hotmail.com>
2016-09-17 13:17:52 -0300

    Renamed new_syntax_test.py to fixture.py

--- tests/test_markdown_light.py
+++ tests/test_markdown_light.py
@@ -1,6 +1,6 @@
-import new_syntax_test
+import fixture
 
-class TestMarkdownLight(new_syntax_test.SyntaxTestCase):
+class TestMarkdownLight(fixture.SyntaxTestCase):
     def setUp(self):
         super().setUp()
         self.set_syntax_file("Packages/MarkdownLight/MarkdownLight.tmLanguage")
